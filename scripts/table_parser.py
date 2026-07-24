01aee8ec2ccf436cfb67d8ba03b605ed689b0d3c
ludarus <burgermanjoe48@gmail.com>
2026-07-24 09:49:24 -0400

    empty imgNum table

--- scripts/table_parser.py
+++ scripts/table_parser.py
@@ -46,8 +46,8 @@ def parse_file(filePath: str) -> list[int]:
     return indicies
 
 
-def convert_to_coords(indicies: list[int]) -> list[tuple]:
-    coords: list[tuple] = []
+def convert_to_coords(indicies: list[int]) -> list[tuple[int]]:
+    coords: list[tuple[int]] = []
     for num in indicies:
         # getting coordinates from global index
         x = num % OLD_WIDTH
@@ -62,18 +62,29 @@ def convert_to_coords(indicies: list[int]) -> list[tuple]:
     return coords
 
 
+# scrap this entire freaking thing
+def generate_c_array(coords: list[tuple[int]]) -> list[str]:
+    carray: list[str] = []
+    carray.append(f"static const Obj_t objects[{len(coords)}] = " + "{")
+    for c in range(len(coords)):
+        # padding the digits of each number for better formatting
+        carray.append(
+            "   {"
+            + f"{(c + 1):03d}, {coords[c][0]:03d}, {coords[c][1]:03d}, &File_{(c+2):03d}_ObjNum_{(c+1):03d}_"
+            + "},"
+        )
+    carray.append("};")
+
+    return "\n".join(carray)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("inputFile", help="a .trc file to emulate")
 
     args = parser.parse_args()
 
-    out = parse_file(args.inputFile)
-
-    print(out)
-    print(len(out))
-
-    print(convert_to_coords(out))
+    print(generate_c_array(convert_to_coords(parse_file(args.inputFile))))
 
     # checking if input is a file
     if not os.path.isfile(args.inputFile):
